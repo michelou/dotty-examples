c70b28202b0bfdf5c602fe0419c220fa6f8ce2b7
Stephane MICHELOUD <stephane.micheloud@gmail.com>
2020-03-06 12:14:31 +0100

    fixed specs2 test

--- examples/enum-Color/src/test/scala/ColorSpecs2Test.scala
+++ examples/enum-Color/src/test/scala/ColorSpecs2Test.scala
@@ -3,15 +3,16 @@
 import org.junit.runner.RunWith
 import org.specs2.mutable.Specification
 import org.specs2.runner.JUnitRunner
-/*
+
 import scala.language.implicitConversions
 
 @RunWith(classOf[JUnitRunner])
 class ColorSpec2Test extends Specification {
-  def is = "This is a specification of enumeration Color"
+  override def is = "This is a specification of enumeration Color"
 
-  "Color enumeration contains 4 elements" >> {
-    Color.values.size must_== 4
+  "Color enumeration" should {
+    "contains 4 elements" in {
+	  Color.values.size must_== 4
+	}
   }
 }
-*/
